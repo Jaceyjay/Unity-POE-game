c9097eacd4c427f4ee20f9deaabeea0f3cb8ec14
Jaceyjay <43611056+Jaceyjay@users.noreply.github.com>
2018-10-25 15:55:37 +0200

    Add files via upload
    Generates resources for both teams as an Int to do calculations

---
+++ ResourceGenerate.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ResourceGenerate : MonoBehaviour {
+    public int counter;
+    public Text ResourceText;
+    public int Resources;
+    public int team;
+
+    // Use this for initialization
+    void Start () {
+       
+        Resources = Resources + Random.Range(0, 6);
+        InvokeRepeating("IncreaseResources", 0f, 3.0f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+       
+            
+	}
+
+    void IncreaseResources()
+    {
+        counter++;
+        Resources = Resources + Random.Range(0, 6);
+        Resources.ToString();
+        counter = 0;
+        ResourceText.text = "Team " + team.ToString() + " Resources: " + Resources;
+    }
+    public void DeductResources()
+    {
+
+    }
+}
